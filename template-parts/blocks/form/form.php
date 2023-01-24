6f501905d717998b8a4aec603fe2d4c23d0206b5
jofbrvega <johanna.vega@medieinstitutet.se>
2023-01-24 13:44:55 +0100

    Fixing form on form

--- template-parts/blocks/form/form.php
+++ template-parts/blocks/form/form.php
@@ -3,17 +3,13 @@ $form_heading = get_field('form_heading');
 $form_shortcode = get_field('form_shortcode');
 ?>
 
-<section class="flex flex-row p-8 mx-auto">
-    <div class="flex justify-center w-full py-8">
-        <div class="flex flex-col justify-center w-full lg:w-1/2">
-            <h1 class="lg:max-w-3/4"><?= $form_heading; ?></h1>
+<section class="w-full max-w-screen-xl p-4 mx-auto">
+    <div class="grid grid-cols-2 gap-10">
+        <div class="flex flex-col justify-center p-4">
+            <h2 class=""><?= $form_heading; ?></h2>
         </div>
-    </div>
-    <div class="flex flex-col justify-center w-full py-8 text-center lg:flex-row">
-        <div class="flex flex-col justify-center w-full lg:w-1/2">
-            <div class="">
+        <div class="p-4">
                 <?php echo do_shortcode($form_shortcode); ?>
-            </div>
         </div>
     </div>
 </section>
