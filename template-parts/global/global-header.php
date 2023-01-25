7dbf7b2031d5c7957dd22dcee729969d7a12689d
jofbrvega <johanna.vega@medieinstitutet.se>
2023-01-25 13:49:05 +0100

    Added last CSS changes

--- template-parts/global/global-header.php
+++ template-parts/global/global-header.php
@@ -9,8 +9,8 @@ $global_logotype = get_field('global_logotype', 'options');
                 <img class="flex h-12" src="<?= esc_url($global_logotype['url']); ?>" alt="<?= esc_url($global_logotype['alt']); ?>">
             </a>
         <?php endif; ?>
-        <button type="button" aria-label="toggle-menu" @click="isOpen = !isOpen" class="relative z-30 flex flex-col justify-between w-10 h-5 focus:outline-none top-1 open">
-            <img class="h-10" src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/icon-hamburger-menu.png" alt="hamburger-menu">
+        <button type="button" aria-label="toggle-menu" @click="isOpen = !isOpen" class="relative z-30 flex flex-col justify-between h-12 focus:outline-none top-1 open">
+            <img class="flex h-12" src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/icon-hamburger-menu.png" alt="hamburger-menu">
         </button>
         <div x-show="isOpen" x-transition x-transition:enter.duration.400ms x-transition:leave.duration.300ms class="fixed inset-y-0 right-0 z-20 flex justify-end visible w-full transition duration-500 ease-in-out bg-black opacity-100 flex-coll bg-opacity-60 open">
             <div class="flex flex-col text-center justify-center w-full transition duration-500 ease-in-out translate-x-0 bg-[#b4b8a5] md:w-1/2">
@@ -24,10 +24,10 @@ $global_logotype = get_field('global_logotype', 'options');
                             $header_menu_top_links_target = $header_menu_top_links['target'] ? $header_menu_top_links['target'] : '_self';
                         endif; ?>
                         <div class="flex justify-center my-2 text-center">
-                        <a href=" <?= esc_url($header_menu_top_links_url); ?>" target="<?= esc_attr($header_menu_top_links_target); ?>" class="flex items-center h-full text-center text-white">
-                            <?= esc_html($header_menu_top_links_title); ?>
-                        </a>
-                    </div>
+                            <a href=" <?= esc_url($header_menu_top_links_url); ?>" target="<?= esc_attr($header_menu_top_links_target); ?>" class="flex items-center h-full text-center text-white">
+                                <?= esc_html($header_menu_top_links_title); ?>
+                            </a>
+                        </div>
                 <?php
                     endwhile;
                 endif;
